a3020cc0645586a953a8c7aabd255dc8723c72b0
rogerramossilva <33642941+rogerramossilva@users.noreply.github.com>
2020-04-14 19:02:39 -0300

    Add files via upload

--- site.pp
+++ site.pp
@@ -1,3 +1,7 @@
+
+class web {
+
+
 exec { "apt-update":
   command => "/usr/bin/apt-get update"
 }
@@ -15,6 +19,7 @@ package {[
     "wget",
     "curl",
     "vim",
+    "zip",
   ]:
   ensure => installed,
   require => Exec["apt-update"],
@@ -32,7 +37,7 @@ file { '/etc/apache2/conf-available/direxpress.conf':
 }
 
 file { "/etc/apache2/sites-available/express.conf":
-  nsure => present,
+  ensure => present,
   owner => root,
   group => root,
   mode => '0664',
@@ -41,3 +46,81 @@ file { "/etc/apache2/sites-available/express.conf":
   require => Package["apache2"],
 #  notify => Service['apache2'],
 }
+
+file { "/etc/hosts":
+  ensure => present,
+  owner => root,
+  group => root,
+  mode => '0664',
+  replace => true,
+  content => template("/vagrant/manifests/hostsintranet.conf"),
+}
+
+file { '/srv/www':
+  ensure => 'directory',
+  owner => 'root',
+  group => 'www-data',
+  mode => '2750',
+}
+
+
+exec { "wget-express.zip":
+  cwd => '/tmp',
+  command => "/usr/bin/wget --no-check-certificate https://github.com/rogerramossilva/devops/raw/master/express.zip",
+  creates => "/tmp/express.zip",
+  require => Package["wget"],
+ # refreshonly => true,
+}
+
+exec { "unzip":
+  cwd => "/srv/www",
+  command => "/usr/bin/unzip /tmp/express.zip -d /srv/www",
+  require => Package["zip"],
+#  refreshonly => true,
+}
+
+
+#archive { '/tmp/express.zip':
+#  source => 'https://github.com/rogerramossilva/devops/raw/master/express.zip',
+#  extract => true,
+#  extract_path => '/srv/www',
+#  user => 'root',
+#  group => 'www-data',
+#  cleanup => false,
+#}
+
+service { 'apache2':
+ ensure => running,
+ enable => true,
+ hasrestart => true,
+ restart => true,
+ require => Package['apache2'],
+}
+
+
+exec { 'a2ensite express':
+  path => ['/usr/bin', '/usr/sbin',],
+  provider => shell,
+  require => Service["apache2"],
+}
+exec { 'a2enconf direxpress':
+  path => ['/usr/bin', '/usr/sbin',],
+  provider => shell,
+  require => Service["apache2"],
+}
+
+exec { 'a2enmod vhost_alias':
+  path => ['/usr/bin', '/usr/sbin',],
+  provider => shell,
+  require => Service["apache2"],
+}
+exec { 'a2enmod php7.0':
+  path => ['/usr/bin', '/usr/sbin',],
+  provider => shell,
+  require => Service["apache2"],
+}
+
+}
+
+include web
+
